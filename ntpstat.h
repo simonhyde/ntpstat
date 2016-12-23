7a0558dc52c16b59125fa534e5d6d60740478be8
Simon Hyde <simon@icedrop.net>
2016-12-23 09:57:47 +0000

    Make compatible with C++

--- ntpstat.h
+++ ntpstat.h
@@ -1,5 +1,8 @@
 #ifndef __NTPSTAT_H_HEADER
 #define __NTPSTAT_H_HEADER
+#ifdef __cplusplus
+extern "C" {
+#endif
 typedef struct
 {
 	int status; /* 0 synch, 1 unsynch, 2 unknown */
@@ -9,4 +12,7 @@ typedef struct
 } ntpstate_t;
 void init_ntp_state();
 int get_ntp_state (ntpstate_t * data);
+#ifdef __cplusplus
+}
+#endif
 #endif
